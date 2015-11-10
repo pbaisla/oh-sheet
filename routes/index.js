f3b33062aa0e33eec59de237fc1ea535352b0582
Prashant Baisla <prashantbaisla@gmail.com>
2015-11-10 21:25:51 +0530

    Create basic sheet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var Sheet = require('../models/sheet');
+
+/* Generates unique URL */
+function randomValueHex() {
+    return crypto.randomBytes(Math.ceil(6))
+        .toString('hex')
+        .slice(0,12);
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var newurl = randomValueHex();
+  res.redirect('/sheet/' + newurl);
+});
+
+router.get('/sheet/:url', function(req, res, next) {
+  var url = req.params.url;
+  Sheet.find({ url: url }, function(err, sheet) {
+    if(err)
+      res.send(err);
+    res.render('sheet', { sheet: sheet.cells||[] });
+  });
 });
 
 module.exports = router;
