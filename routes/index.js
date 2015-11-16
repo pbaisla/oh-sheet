7d4032363a3779ccd80a573ad0ec5effc510333c
Prashant Baisla <prashantbaisla@gmail.com>
2015-11-17 01:55:23 +0530

    Implement Two Phase Commit Protocol

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,8 @@ router.get('/sheet/:url', function(req, res, next) {
   });
 });
 
+twoPCstates = {};
+
 module.exports = function(io) {
   io.on('connection', function(socket) {
     socket.emit('sheet.url?');
@@ -32,15 +34,50 @@ module.exports = function(io) {
       socket.join(data.url);
     });
 
+    socket.on('requestForCommit', function(data) {
+      twoPCstates[data.id + data.counter] = 0;
+      io.to(data.url).emit('prepareForCommit', data);
+    });
+
+    socket.on('agreement', function(data) {
+      if(twoPCstates.hasOwnProperty(data.id + data.counter)) {
+        twoPCstates[data.id + data.counter]++;
+        if(twoPCstates[data.id + data.counter] == io.to(data.url).sockets.length) {
+          var changes = [];
+          for(var i=0; i < data.changes.length; i++) {
+            changes.push({
+              row: data.changes[i][0],
+              col: data.changes[i][1],
+              data: data.changes[i][3]
+            });
+          }
+          io.to(data.url).emit('commit', changes);
+          delete twoPCstates[data.id + data.counter];
+        }
+      }
+    });
+
+    socket.on('disagreement', function(data){
+      var changes = [];
+      for(var i=0; i < data.changes.length; i++) {
+        changes.push({
+          row: data.changes[i][0],
+          col: data.changes[i][1],
+          data: data.changes[i][2]
+        });
+      }
+      socket.to(data.id).emit('abort', changes);
+      delete twoPCstates[data.id + data.counter];
+    });
+
     socket.on('save', function(data) {
-	io.to(data.url).emit('changes',data.cells);
+      io.to(data.url).emit('changes',data.cells);
       Sheet.findOne(
           { url: data.url },
           function(err, sheet) {
             if(err)
               console.log(err);
             if(sheet) {
-		
               for(var i=0; i < data.cells.length; i++) {
                 var ind = sheet.cells.findIndex(function(element, index, array){
                   if((element.row == data.cells[i].row) && (element.col == data.cells[i].col))
